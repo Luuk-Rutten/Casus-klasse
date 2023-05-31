f01b56e049fa81c72ba8dc8a35d041c5f4eba146
Luuk <Luuk@DESKTOP-3RSSFQS>
2023-05-31 11:58:26 +0200

    Add project files.

---
+++ DataVerwerking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casus_klasse
+{
+    internal class DataVerwerking
+    {
+
+
+
+        //bestand opslaan en als bestand niet bestaat nieuw project aanmaken, geen extra klasse voor aanmaken nodig.
+        public void Opslaan()
+        {
+
+        }
+
+        public void Verwijderen()
+        {
+
+
+        }
+
+        public object Uitlezen()
+        {
+
+            return 0; 
+        }
+    
+    }
+
+}
---
+++ Models.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Casus_klasse
+{
+    internal class Models
+    {
+    }
+
+    public class Personeel
+    {
+        public string naam;
+        public string? functie;
+        public int ID;
+
+    }
+
+    public class Project
+    {
+        public string ProjectNaam { get; set; }
+        public DateTime StartDatum { get; set; }
+        public DateTime EindDatum { get; set; }
+
+        public TaakToevoegen()
+        {
+            return;
+        }
+
+        public NieuwProject()
+        {
+            return;
+        }
+        public SaveProject()
+        {
+            return;
+        }
+        public OpenProject()
+        {
+            return;
+        }
+        public DeleteProject()
+        {
+            return;
+        }
+
+        public class Taak
+       {
+            public int TaakNummer;
+
+            public void VoegPersoneelToe()
+            {
+                 
+            }
+
+
+            public AddTaak()
+            {
+                return;
+            }
+            public DeleteTaak()
+            {
+                return;
+            }
+        }
+
+        public class Bestand
+        {
+            public OpenBestand()
+            {
+
+                return;
+            }
+            public SluitBestand()
+            {
+
+                return;
+            }
+            public SaveBestand()
+            {
+
+                return;
+            }
+            public DeleteBestand()
+            {
+
+                return;
+            }
+
+        }
+        public class Data
+        {
+            public ReadData()
+            {
+                return;
+            }
+            public SaveData()
+            {
+                return;
+            }
+
+
+        }
+
+    }
+}
+
+
